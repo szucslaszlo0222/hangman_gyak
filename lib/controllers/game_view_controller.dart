2acfb3cd442377066056d70620c92f04aa960d76
diak <diak@mesterlevel.lo>
2023-10-17 11:57:09 +0200

    controller bővítve lett

--- lib/controllers/game_view_controller.dart
+++ lib/controllers/game_view_controller.dart
@@ -1,4 +1,39 @@
 // játék logika
-import 'package:get/get.dart';
+import 'dart:math';
 
-class GameViewController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:hangman/models/tipp.dart';
+
+class GameViewController extends GetxController {
+  List<String> szovegek = [
+    "alma piros",
+    "az ég kék",
+    "gurul a szekér",
+  ];
+  String randomSzoveg = "";
+
+  String csillagosSzoveg = "";
+
+  void randomSzovegGeneralas(){
+    int index =Random().nextInt(szovegek.length);
+    randomSzoveg = szovegek[index];
+
+    for(int i = 0 ; i < randomSzoveg.length; i++){
+      csillagosSzoveg +="*";
+
+    }
+    update();
+  }
+  List<Tipp> tippek =[];
+
+bool nyerteEaFelhasznalo(){
+  return csillagosSzoveg == randomSzoveg;
+}
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    randomSzovegGeneralas();
+  }
+}
